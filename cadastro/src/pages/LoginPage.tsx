a13f00e225cdd54f0956a35146aee1a7d0574de4
Pedro <dev.pedrolucca@gmail.com>
2022-10-25 09:53:24 -0300

    cadastro / login : frontend 1

--- cadastro/src/pages/LoginPage.tsx
+++ cadastro/src/pages/LoginPage.tsx
@@ -1,5 +1,11 @@
 export default function () {
   return <>
-    <h1>teste</h1>
+    <h1>Cadastre-se</h1>
+    
+    <form className="cadastro-form">
+        <input type="text" />
+        <input type="password" name="" id="" />
+        <button>Cadastrar-se</button>
+    </form>
   </>
 }
